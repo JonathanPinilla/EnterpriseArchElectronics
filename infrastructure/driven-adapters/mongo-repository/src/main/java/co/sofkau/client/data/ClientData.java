4954788ca48c5c048e97f136b582513c173a8434
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-04-04 12:55:25 -0500

    Client Validations implemented

--- infrastructure/driven-adapters/mongo-repository/src/main/java/co/sofkau/client/data/ClientData.java
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/sofkau/client/data/ClientData.java
@@ -1,11 +1,16 @@
 package co.sofkau.client.data;
 
 import co.sofkau.model.order.Order;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -20,13 +25,29 @@ public class ClientData {
 
     @Id
     private String id;
+
+    @NotNull(message = "Name is required")
+    @Size(min = 1, max = 15, message = "Name should be between 1 and 15 characters")
+    @Pattern(regexp = "^[a-zA-Z]*$", message = "Name should contain only alphabets")
     private String name;
+
+    @NotNull(message = "Last name is required")
+    @Size(min = 1, max = 15, message = "Name should be between 1 and 15 characters")
+    @Pattern(regexp = "^[a-zA-Z]*$", message = "Last Name should contain only alphabets")
     private String lastName;
+
+    @NotNull(message = "Birth date is required")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
+
+    @NotNull(message = "Email is required")
+    @Email(message = "Email is not valid")
     private String email;
+
+    @NotNull(message = "Password is required")
     private String password;
-    private List<Order> orders;
-    private Boolean deleted;
+    private List<Order> orders = new ArrayList<>();
+    private Boolean deleted = false;
 
     public ClientData(String name, String lastName, LocalDate birthDate, String email, String password) {
         this.id = UUID.randomUUID().toString();
