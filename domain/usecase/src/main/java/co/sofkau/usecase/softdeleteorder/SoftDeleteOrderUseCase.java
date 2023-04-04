42574eaa6e5bfd60ef793d0106f94ce0fdf598e3
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-04-03 23:06:27 -0500

    New Order Use Cases implemented

---
+++ domain/usecase/src/main/java/co/sofkau/usecase/softdeleteorder/SoftDeleteOrderUseCase.java
@@ -0,0 +1,18 @@
+package co.sofkau.usecase.softdeleteorder;
+
+import co.sofkau.model.order.gateways.OrderGateway;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
+@RequiredArgsConstructor
+public class SoftDeleteOrderUseCase implements Function<String, Mono<Void>> {
+
+    private final OrderGateway orderGateway;
+
+    @Override
+    public Mono<Void> apply(String id) {
+        return orderGateway.softDelete(id);
+    }
+}
