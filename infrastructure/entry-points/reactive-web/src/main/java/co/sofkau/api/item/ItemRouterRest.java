fbbe464cdd9a622aa46ace81a3700a2ea2413d74
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-04-04 15:53:24 -0500

    item swagger config id example change

--- infrastructure/entry-points/reactive-web/src/main/java/co/sofkau/api/item/ItemRouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/sofkau/api/item/ItemRouterRest.java
@@ -20,7 +20,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.BodyExtractor;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
