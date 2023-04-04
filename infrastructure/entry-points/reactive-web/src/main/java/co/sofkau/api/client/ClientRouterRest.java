4756dd952e89d7c4fe077f008c6ea541711468b3
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-04-04 15:23:12 -0500

    Client model Swagger doc implemented

--- infrastructure/entry-points/reactive-web/src/main/java/co/sofkau/api/client/ClientRouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/sofkau/api/client/ClientRouterRest.java
@@ -7,6 +7,14 @@ import co.sofkau.usecase.getclientbyid.GetClientByIdUseCase;
 import co.sofkau.usecase.saveclient.SaveClientUseCase;
 import co.sofkau.usecase.softdeleteclient.SoftDeleteClientUseCase;
 import co.sofkau.usecase.updateclient.UpdateClientUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -22,6 +30,15 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class ClientRouterRest {
 
     @Bean
+    @RouterOperation(path = "/api/clients", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = GetAllClientsUseCase.class,
+            beanMethod = "get",
+            operation = @Operation(operationId = "getAllClients", tags = "Client use cases", responses = {
+                    @ApiResponse(responseCode = "200", description = "Successful operation"),
+                    @ApiResponse(responseCode = "400", description = "Bad request")
+            })
+    )
     public RouterFunction<ServerResponse> getAllClients(GetAllClientsUseCase getAllClientsUseCase) {
         return route(GET("/api/clients"),
                 request -> ServerResponse.ok()
@@ -31,6 +48,25 @@ public class ClientRouterRest {
     }
 
     @Bean
+    @RouterOperation(path = "/api/clients/{id}", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = GetClientByIdUseCase.class,
+            beanMethod = "apply",
+            operation = @Operation(operationId = "getClientById", tags = "Client use cases",
+                    parameters = {
+                            @Parameter(
+                                    name = "id",
+                                    description = "The id of the client",
+                                    required = true,
+                                    in = ParameterIn.PATH,
+                                    example = "642c56199cee5868b26f6cf5"
+                            )
+                    },
+                    responses = {
+                            @ApiResponse(responseCode = "200", description = "Successful operation"),
+                            @ApiResponse(responseCode = "400", description = "Bad request")
+                    })
+    )
     public RouterFunction<ServerResponse> getClientById(GetClientByIdUseCase getClientByIdUseCase) {
         return route(GET("/api/clients/{id}"),
                 request -> getClientByIdUseCase.apply(request.pathVariable("id"))
@@ -41,6 +77,27 @@ public class ClientRouterRest {
     }
 
     @Bean
+    @RouterOperation(path = "/api/clients", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = SaveClientUseCase.class,
+            beanMethod = "apply",
+            operation = @Operation(operationId = "saveClient", tags = "Client use cases",
+                    parameters = {
+                            @Parameter(
+                                    name = "client",
+                                    description = "The client to save",
+                                    in = ParameterIn.PATH,
+                                    schema = @Schema(implementation = Client.class)
+                            )
+                    },
+                    responses = {
+                            @ApiResponse(responseCode = "201", description = "Successful operation"),
+                            @ApiResponse(responseCode = "406", description = "Not acceptable")
+                    },
+                    requestBody = @RequestBody(
+                            required = true,
+                            content = @Content(schema = @Schema(implementation = Client.class))
+                    )))
     public RouterFunction<ServerResponse> saveClient(SaveClientUseCase saveClientUseCase) {
         return route(POST("/api/clients").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(Client.class)
@@ -52,6 +109,29 @@ public class ClientRouterRest {
     }
 
     @Bean
+    @RouterOperation(path = "/api/clients/{id}", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = DeleteClientUseCase.class,
+            beanMethod = "apply",
+            operation = @Operation(operationId = "deleteClient", tags = "Client use cases",
+                    parameters = {
+                            @Parameter(
+                                    name = "id",
+                                    description = "The id of the client",
+                                    required = true,
+                                    in = ParameterIn.PATH,
+                                    example = "642c56199cee5868b26f6cf5"
+                            )
+                    },
+                    responses = {
+                            @ApiResponse(responseCode = "200", description = "Successful operation"),
+                            @ApiResponse(responseCode = "400", description = "Bad request")
+                    },
+                    requestBody = @RequestBody(
+                            required = true,
+                            content = @Content(schema = @Schema(implementation = Client.class))
+                    ))
+    )
     public RouterFunction<ServerResponse> updateClient(UpdateClientUseCase updateClientUseCase) {
         return route(PUT("/api/clients/{id}").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(Client.class)
@@ -63,6 +143,25 @@ public class ClientRouterRest {
     }
 
     @Bean
+    @RouterOperation(path = "/api/clients/{id}", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = SoftDeleteClientUseCase.class,
+            beanMethod = "apply",
+            operation = @Operation(operationId = "softDeleteClient", tags = "Client use cases",
+                    parameters = {
+                            @Parameter(
+                                    name = "id",
+                                    description = "The id of the client to soft delete",
+                                    required = true,
+                                    in = ParameterIn.PATH,
+                                    example = "642c56199cee5868b26f6cf5"
+                            )
+                    },
+                    responses = {
+                            @ApiResponse(responseCode = "200", description = "Successful operation"),
+                            @ApiResponse(responseCode = "400", description = "Bad request")
+                    })
+    )
     public RouterFunction<ServerResponse> softDeleteClient(SoftDeleteClientUseCase softDeleteClientUseCase) {
         return route(DELETE("/api/clients/{id}"),
                 request -> softDeleteClientUseCase.apply(request.pathVariable("id"))
@@ -73,6 +172,25 @@ public class ClientRouterRest {
     }
 
     @Bean
+    @RouterOperation(path = "/api/clients/delete/{id}", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = DeleteClientUseCase.class,
+            beanMethod = "apply",
+            operation = @Operation(operationId = "deleteClient", tags = "Client use cases",
+                    parameters = {
+                            @Parameter(
+                                    name = "id",
+                                    description = "The id of the client to delete",
+                                    required = true,
+                                    in = ParameterIn.PATH,
+                                    example = "642c56199cee5868b26f6cf5"
+                            )
+                    },
+                    responses = {
+                            @ApiResponse(responseCode = "200", description = "Successful operation"),
+                            @ApiResponse(responseCode = "400", description = "Bad request")
+                    })
+    )
     public RouterFunction<ServerResponse> deleteClient(DeleteClientUseCase deleteClientUseCase) {
         return route(DELETE("/api/clients/delete/{id}"),
                 request -> deleteClientUseCase.apply(request.pathVariable("id"))
